e9ce74d9ee2fcdb2e50fcda84eb582d6d9b9431f
Liu Yuansan <lysarthas@gmail.com>
2022-07-09 10:56:55 +1000

    clean code

--- contrastive.py
+++ contrastive.py
@@ -5,24 +5,18 @@ from tensorflow.python.ops.variables import trainable_variables
 class ContrastiveModel(tf.keras.Model):
     def __init__(self, encoder, projector):
         super(ContrastiveModel, self).__init__()
-        # self.classifier = classifier
         self.encoder = encoder
         self.projector = projector
-        # self.c_steps = c_steps
 
     def compile(self, e_optimizer, e_loss_fn):
         super(ContrastiveModel, self).compile()
-        # self.c_optimizer = c_optimizer
         self.e_optimizer = e_optimizer
-        # self.c_loss_fn = c_loss_fn
         self.e_loss_fn = e_loss_fn
-        # self.c_loss_metrics = tf.keras.metrics.Mean(name='c_loss')
         self.e_loss_metrics = tf.keras.metrics.Mean(name='e_loss')
 
     def summary(self):
         self.encoder.summary()
         self.projector.summary()
-        # self.classifier.summary()
 
     @property
     def metrics(self):
@@ -30,7 +24,6 @@ class ContrastiveModel(tf.keras.Model):
 
     def train_step(self, data):
         raw_ts, label = data
-        # batch_size = tf.shape(raw_ts)[0]
 
         with tf.GradientTape() as tape:
             latent_codes = self.encoder(raw_ts, training = True)
@@ -41,22 +34,9 @@ class ContrastiveModel(tf.keras.Model):
         self.e_optimizer.apply_gradients(
             zip(grads, trainable_weights)
         )
-
-        # for _ in range(self.c_steps):
-        #     with tf.GradientTape() as tape:
-        #         features = self.encoder(raw_ts, training = True)
-        #         predictions = self.classifier(features, training = True)
-        #         c_loss = self.c_loss_fn(label, predictions)
-        #     grads = tape.gradient(c_loss, self.classifier.trainable_weights)
-        #     self.c_optimizer.apply_gradients(
-        #         zip(grads, self.classifier.trainable_weights)
-        #     )
-
-        # self.c_loss_metrics.update_state(c_loss)
         self.e_loss_metrics.update_state(e_loss)
 
         return {
-            # "c_loss": self.c_loss_metrics.result(),
             "e_loss": self.e_loss_metrics.result(),
         }
 
@@ -64,24 +44,18 @@ class ContrastiveModel(tf.keras.Model):
 class ContrastiveModel_ori(tf.keras.Model):
     def __init__(self, encoder, projector):
         super(ContrastiveModel_ori, self).__init__()
-        # self.classifier = classifier
         self.encoder = encoder
         self.projector = projector
-        # self.c_steps = c_steps
 
     def compile(self, e_optimizer, e_loss_fn):
         super(ContrastiveModel_ori, self).compile()
-        # self.c_optimizer = c_optimizer
         self.e_optimizer = e_optimizer
-        # self.c_loss_fn = c_loss_fn
         self.e_loss_fn = e_loss_fn
-        # self.c_loss_metrics = tf.keras.metrics.Mean(name='c_loss')
         self.e_loss_metrics = tf.keras.metrics.Mean(name='e_loss')
 
     def summary(self):
         self.encoder.summary()
         self.projector.summary()
-        # self.classifier.summary()
 
     @property
     def metrics(self):
@@ -89,8 +63,6 @@ class ContrastiveModel_ori(tf.keras.Model):
 
     def train_step(self, data):
         raw_ts, label = data
-        # aug1 = raw_ts[:, 0, :, :]
-        # aug2 = raw_ts[:, 1, :, :]
         batch_size = tf.shape(raw_ts)[0]
         num_views = tf.shape(raw_ts)[1]
         ts_shape = tf.concat(([batch_size*num_views], tf.shape(raw_ts)[2:]), axis=0)
@@ -108,21 +80,9 @@ class ContrastiveModel_ori(tf.keras.Model):
             zip(grads, trainable_weights)
         )
 
-        # for _ in range(self.c_steps):
-        #     with tf.GradientTape() as tape:
-        #         features = self.encoder(raw_ts, training = True)
-        #         predictions = self.classifier(features, training = True)
-        #         c_loss = self.c_loss_fn(label, predictions)
-        #     grads = tape.gradient(c_loss, self.classifier.trainable_weights)
-        #     self.c_optimizer.apply_gradients(
-        #         zip(grads, self.classifier.trainable_weights)
-        #     )
-
-        # self.c_loss_metrics.update_state(c_loss)
         self.e_loss_metrics.update_state(e_loss)
 
         return {
-            # "c_loss": self.c_loss_metrics.result(),
             "e_loss": self.e_loss_metrics.result(),
         }
         
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Dropout, Conv1D, LSTM, LayerNormalization, MultiHeadAttention, SpatialDropout1D, Flatten, GlobalAveragePooling1D
+from tensorflow.keras.layers import Input, Dense, Dropout, Conv1D, LayerNormalization, MultiHeadAttention, SpatialDropout1D, GlobalAveragePooling1D
 
 def cautrans_enc(input_shape, head_size, num_heads, num_f, dilations, k_size, dropout=0.0):
     inputs = Input(shape=input_shape)
@@ -11,7 +11,7 @@ def cautrans_enc(input_shape, head_size, num_heads, num_f, dilations, k_size, dr
     x = Dropout(dropout)(x)
     x = LayerNormalization(epsilon=1e-6)(x)
     res = x + inputs
-
+    # TCN
     x = res
     for d in dilations:
         x = Conv1D(filters=num_f, kernel_size=k_size, dilation_rate=d, padding='causal', activation='relu')(x)
@@ -19,35 +19,8 @@ def cautrans_enc(input_shape, head_size, num_heads, num_f, dilations, k_size, dr
         x = LayerNormalization(epsilon=1e-06)(x)
     x = x + res
     # Map to latent space
-    # x = Flatten()(x)
-    # outputs = Dense(input_shape[0])(x)
     outputs = GlobalAveragePooling1D(data_format='channels_first')(x)
     return Model(inputs, outputs, name='encoder')
-
-def transformer_block(inputs, head_size, num_heads, ff_dim, dropout=0):
-    # Attention and Normalization
-
-    x = MultiHeadAttention(
-        key_dim=head_size, num_heads=num_heads, dropout=dropout
-    )(inputs, inputs)
-    x = Dropout(dropout)(x)
-    x = LayerNormalization(epsilon=1e-6)(x)
-    res = x + inputs
-
-    # Feed Forward Part
-    x = Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(res)
-    x = Dropout(dropout)(x)
-    x = Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
-    x = LayerNormalization(epsilon=1e-6)(x)
-    return x+res
-
-def encoder(input_shape, num_block, head_size, num_heads, ff_dim, dropout):
-    inputs = Input(shape=input_shape)
-    x = inputs
-    for _ in range(num_block):
-        x = transformer_block(x, head_size, num_heads, ff_dim, dropout)
-    output = GlobalAveragePooling1D(data_format="channels_first")(x)
-    return Model(inputs, output)
 
 def projector(input_shape, mlp_units, mlp_dropout=0):
     inputs = Input(shape=input_shape)
